20cd0551ab0f67a5218f1455ff7dd629be1f5fbe
Justin Nguyen <jnguyenblue2804@gmail.com>
2022-08-24 09:27:05 -0500

    Initial commit

---
+++ justin_nguyen_unmanned_systems/week_1/practice.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 24 09:20:42 2022
+
+@author: jnguy
+"""
+
+
+
+#% Import stuff here
+
+
+
+
+#% Classes 
+
+
+
+
+#% Main 
+
+if __name__=='__main__':
+    print("hello world")
